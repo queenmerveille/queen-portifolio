f9f7a9eb2be991c77461c5e791ffd8ee6b283916
Germain Hirwa <higermain1@gmail.com>
2024-12-26 05:33:04 -0600

    improved the UI

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ const Home = () => {
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
-        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+        <div className="flex flex-col xl:flex-row items-center justify-between pt-2 xl:pt-2 xl:pb-12">
           {/* text */}
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">High-school Senior & Software Developer</span>
@@ -18,10 +18,7 @@ const Home = () => {
               Hello, I'm <br /> <span className="text-accent">Queen Umuhoza</span>
             </h1>
             <p className="max-w-[600px] mb-9 text-white/80">
-              Passionate Senior Highschool student at Fawe Girls School, 
-              with a track record of: Leadership, public Speaking and innovative software development. I blend academic 
-              rigor with practical experience to create impactful solutions, 
-              from mobile apps to enterprise software.
+              Passionate Senior Highschool student at Fawe Girls School, with a track record of: Leadership, public Speaking and innovative software development. I blend academic rigor with practical experience to create impactful solutions, from student leadership initiatives to technology innovations.
             </p>
             {/* btn and socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
@@ -35,7 +32,7 @@ const Home = () => {
                   <FiDownload className="text-xl" />
                 </Button>
               </a>
-              <div className="mb-8 xl:mb-0">
+              <div className="mb-6 xl:mb-0">
                 <Social
                   containerStyles="flex gap-6"
                   iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
@@ -44,12 +41,14 @@ const Home = () => {
             </div>
           </div>
           {/* photo */}
-          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+          <div className="order-1 xl:order-none mb-6 xl:mb-0">
             <Photo />
           </div>
         </div>
       </div>
-      <Stats />
+      <div className="-mt-8 xl:-mt-0">
+        <Stats />
+      </div>
     </section>
   );
 };
